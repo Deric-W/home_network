434bf4ee52aef3bb2d67c2e9e0fcf3f76c1a28a2
Eric Wolf <robo-eric@gmx.de>
2022-09-15 20:49:55 +0200

    add encrypted partition to mini-me

--- hosts/mini-me/hardware.nix
+++ hosts/mini-me/hardware.nix
@@ -6,12 +6,15 @@
 
   boot = {
     kernelPackages = pkgs.linuxPackages_rpi4;
-    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+    initrd= {
+      availableKernelModules = [ "usbhid" "usb_storage" ];
+    };
     kernelParams = [
       "8250.nr_uarts=1"
       "console=ttyAMA0,115200"
       "console=tty1"
       "cma=128M"
+      "boot.shell_on_fail"
     ];
     kernel.sysctl = {
       "vm.swappiness" = 10;
@@ -36,6 +39,17 @@
       device = "/dev/disk/by-label/data";
       fsType = "ext4";
       options = [ "defaults" "noatime" ];
+    };
+    "/vault" = {
+      device = "/dev/mapper/vault";
+      fsType = "ext4";
+      options = [ "defaults" "noatime" ];
+      encrypted = {
+        enable = true;
+        label = "vault";
+        keyFile = "/mnt-root/secrets/vault.key";
+        blkDev = "/dev/disk/by-id/usb-TOSHIBA_External_USB_3.0_20180716002134C-0:0-part5";
+      };
     };
   };
 
