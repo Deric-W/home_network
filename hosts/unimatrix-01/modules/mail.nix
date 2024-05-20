5745abfa849c5518d4370737892743d2d0835120
Eric Wolf <robo-eric@gmx.de>
2024-05-20 06:31:04 +0200

    add borgmatic configuration for mail

--- hosts/unimatrix-01/modules/mail.nix
+++ hosts/unimatrix-01/modules/mail.nix
@@ -1,4 +1,5 @@
 { config, ... }:
+with builtins;
 {
   mailserver = {
     enable = true;
@@ -56,5 +57,49 @@
       reloadUnits = [ "dovecot2.service" ];
       sopsFile = ../../../secrets/dovecot.yaml;
     };
+  };
+
+  services.borgmatic.configurations.mail = {
+    source_directories = [
+      config.mailserver.mailDirectory
+      config.mailserver.sieveDirectory
+      config.mailserver.dkimKeyDirectory
+      "/var/lib/rspamd"
+      "/var/lib/redis-rspamd/dump.rdb"
+    ];
+    exclude_patterns = [
+      "${config.mailserver.mailDirectory}/*/*/tmp"
+      "${config.mailserver.mailDirectory}/*/*/dovecot-uidlist.lock"
+      "/var/lib/rspamd/*.hs"
+      "/var/lib/rspamd/*.hsmp"
+      "/var/lib/rspamd/*.map"
+    ];
+    repositories = [
+      {
+        label = "services repository";
+        path = "ssh://${config.services.borgbackup.repos.services.user}@localhost:${toString (head config.services.openssh.ports)}/${config.services.borgbackup.repos.services.path}";
+      }
+    ];
+    ssh_command = let ed25519key = head (filter (key: key.type == "ed25519") config.services.openssh.hostKeys); in "ssh -i ${ed25519key.path}";
+    source_directories_must_exist = true;
+    archive_name_format = "{hostname}-mail-{now:%Y-%m-%dT%H:%M:%S.%f}";
+    keep_within = "1H";
+    keep_secondly = 0;
+    keep_minutely = 0;
+    keep_hourly = 0;
+    keep_daily = 7;
+    keep_weekly = 4;
+    keep_monthly = 6;
+    keep_yearly = 2;
+    checks = [
+      {
+        name = "repository";
+        frequency = "1 month";
+      }
+      {
+        name = "archives";
+        frequency = "1 month";
+      }
+    ];
   };
 }
