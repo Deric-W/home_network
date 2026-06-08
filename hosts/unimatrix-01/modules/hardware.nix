68d2c1b152c47316622effb663fe4369b307e406
Eric Wolf <robo-eric@gmx.de>
2026-06-08 02:57:12 +0200

    refactor unimatrix-01 hardware config
    - remove unused kernel params
    - move device tree overlay to a separate file (and check for any upsteam updates)
    - disable fkms-3d (was never used and setup is probably broken)

--- hosts/unimatrix-01/modules/hardware.nix
+++ hosts/unimatrix-01/modules/hardware.nix
@@ -1,8 +1,20 @@
 { inputs, pkgs, ... }:
 let
-  btrfs-options = [ "defaults" "noatime" "nodiscard" "barrier" ];
-  xfs-options = [ "defaults" "noatime" "nodiscard" ];
-  vfat-options = [ "defaults" "noatime" ];
+  btrfs-options = [
+    "defaults"
+    "noatime"
+    "nodiscard"
+    "barrier"
+  ];
+  xfs-options = [
+    "defaults"
+    "noatime"
+    "nodiscard"
+  ];
+  vfat-options = [
+    "defaults"
+    "noatime"
+  ];
 in
 {
   boot = {
@@ -21,12 +33,9 @@ in
       in
       crossPkgs.linuxPackagesFor rpi4-kernel;
 
-    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
-    kernelParams = [
-      "8250.nr_uarts=1"
-      "console=ttyAMA0,115200"
-      "console=tty1"
-      "cma=128M"
+    initrd.availableKernelModules = [
+      "usbhid"
+      "usb_storage"
     ];
     kernel.sysctl = {
       "vm.swappiness" = 10;
@@ -34,73 +43,17 @@ in
   };
 
   hardware.enableRedistributableFirmware = true;
-  hardware.raspberry-pi."4" = {
-    fkms-3d.enable = true;
-    apply-overlays-dtmerge.enable = true;
-  };
+  hardware.raspberry-pi."4".apply-overlays-dtmerge.enable = true;
   hardware.deviceTree = {
     enable = true;
     # to prevent build failure with compute module 4 trees
     filter = "bcm2711-rpi-4*.dtb";
-    overlays = [{
-      name = "gpio-fan-overlay";
-      # modified https://github.com/raspberrypi/linux/blob/rpi-6.1.y/arch/arm/boot/dts/overlays/gpio-fan-overlay.dts
-      dtsText = ''
-        /dts-v1/;
-        /plugin/;
-
-        / {
-          compatible = "brcm,bcm2711";
-
-          fragment@0 {
-            target-path = "/";
-            __overlay__ {
-              fan0: gpio-fan@0 {
-                compatible = "gpio-fan";
-                gpios = <&gpio 12 0>;
-                gpio-fan,speed-map = <0    0>,
-                          <5000 1>;
-                #cooling-cells = <2>;
-              };
-            };
-          };
-
-          fragment@1 {
-            target = <&cpu_thermal>;
-            __overlay__ {
-              polling-delay = <2000>;	/* milliseconds */
-            };
-          };
-
-          fragment@2 {
-            target = <&thermal_trips>;
-            __overlay__ {
-              cpu_hot: trip-point@0 {
-                temperature = <60000>;	/* (millicelsius) Fan started at 60°C */
-                hysteresis = <10000>;	/* (millicelsius) Fan stopped at 50°C */
-                type = "active";
-              };
-            };
-          };
-
-          fragment@3 {
-            target = <&cooling_maps>;
-            __overlay__ {
-              map0 {
-                trip = <&cpu_hot>;
-                cooling-device = <&fan0 1 1>;
-              };
-            };
-          };
-
-          __overrides__ {
-            gpiopin = <&fan0>,"gpios:4", <&fan0>,"brcm,pins:0";
-            temp = <&cpu_hot>,"temperature:0";
-            hyst = <&cpu_hot>,"hysteresis:0";
-          };
-        };
-      '';
-    }];
+    overlays = [
+      {
+        name = "gpio-fan-overlay";
+        dtsFile = ./gpio-fan-overlay.dts;
+      }
+    ];
   };
 
   fileSystems = {
